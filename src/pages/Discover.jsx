3ba2648315248c1ebd5fff81d8064cdfcf117c6c
Zack289 <ozack911@gmail.com>
2026-02-06 13:07:04 +0545

    added the error and loading component to the project

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,11 +1,20 @@
-import { Error, Loader, SongCard } from '../components';
-import { genres } from '../assets/constants';
-import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const genreTitle = "Pop";
 
-  const { data, isFetching, error} = useGetTopChartsQuery();
-  const genreTitle = 'Pop';
+  if (isFetching) return <Loader title="Loading Songs..." />;
+  if (error) return <Error />;
+
+  console.log(data);
+
+
+  // Extract tracks safely
+  // const songs = data?.tracks?.data?.map(hit => hit.track) || [];
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -14,7 +23,6 @@ const Discover = () => {
         </h2>
         <select
           onChange={() => {}}
-          value=""
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => (
@@ -26,13 +34,9 @@ const Discover = () => {
       </div>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-          {[1,2,3,4,5,6,7,8].map((song, i)=>(
-            <SongCard 
-            key={song.key}
-            song={song}
-            i={i}
-            />
-          ))}
+        {data?.data?.map((song, i) => (
+          <SongCard key={song.key} song={song} i={i} />
+        ))}
       </div>
     </div>
   );
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,31 +1,36 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// const options = { method: 'GET'};
-
-// fetch('https://api.deezer.com/chart', options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
-
 export const shazamCoreApi = createApi({
-  reducerPath: 'sazamCoreApi',
+  reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.deezer.com/',
-    // prepareHeaders: (headers) => {
-    //   headers.set(
-    //     "X-RapidAPI-Key",
-    //     import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY,
-    //   );
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '63bd7806bcmsh5ab74c06a06b1a5p109ef2jsn63b0cc405dab');
+       headers.set(
+        'X-RapidAPI-Host',
+        'shazam-core.p.rapidapi.com'
+      );
 
-    //   return headers;
-    // },
+      return headers;
+    },
   }),
-
-  endpoints: (builder)=>({
-    getTopCharts: builder.query({ query: () => 'chart' })
-  })
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/search/multi?search_type=SONGS&offset=0&query=pop' }),
+    getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+  }),
 });
 
 export const {
-    useGetTopChartsQuery, 
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
 } = shazamCoreApi;
